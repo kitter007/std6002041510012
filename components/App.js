06cab0db9e5c70f09e9ce636b7bccff03401ba83
กิตติพร สินนุรักษ์กูล <s6002041510012@Macmini-213-12.local>
2019-03-28 14:21:50 +0700

    using Header component on App

--- components/App.js
+++ components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View,Text} from 'react-native';
+import Header from './Header';
 
 class App extends React.Component{
     render(){
         return(
             <View>
+                <Header title = {'Albums'}></Header>
                 <Text>Kittiporn Sinnurukkun</Text>
             </View>
         );
