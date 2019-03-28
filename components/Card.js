9a4c6d1aed2646706bb15aa298fd1ae7fa2c3596
กิตติพร สินนุรักษ์กูล <s6002041510012@Macmini-213-12.local>
2019-03-28 16:14:35 +0700

    add photo Card component

--- components/Card.js
+++ components/Card.js
@@ -1,17 +1,47 @@
 import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+import {View,Text,Image,Button} from 'react-native';
 import CardSection from './CardSection';
 
 class Card extends Component{
     render(){
         return(
             <View>
-                <CardSection/>
-                <CardSection/>
-                <CardSection/>
+                <CardSection>
+                    <Image 
+                        style={{width: 100, height: 100}}
+                        source={{uri: 'https://bit.ly/2HXxf4P'}}/>
+
+                    <Text>พรุ่งนี้ค่อย</Text>
+                    <Text>POP</Text>
+
+                </CardSection>
+                
+                <CardSection>
+                    <Image 
+                        style={{width: 420, height: 350}}
+                        source={{uri: 'https://bit.ly/2HXxf4P'}}/>
+                </CardSection>
+
+                <CardSection>
+                    <View style={styles.button}>
+                    <View style={{width:100}}> 
+                        <Button
+                            title="Buy"
+                            color="red"
+                            />
+                    </View>
+                    </View>
+                </CardSection>
             </View>
         );
     }
 }
-
 export default Card;
+
+const styles = {
+    button: { 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        marginTop: 10 
+    }
+}
