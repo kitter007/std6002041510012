15bda53f84f641ed07a65b023c525aecb9e671e1
กิตติพร สินนุรักษ์กูล <s6002041510012@Macmini-213-12.local>
2019-03-28 15:21:53 +0700

    create CardSection component

---
+++ components/CardSection.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import {View,Text} from 'react-native'
+
+class CardSection extends Component{
+    render(){
+        return(
+            <View>
+                <Text>XXXX</Text>
+            </View>
+        );
+    }
+}
+export default CardSection;
