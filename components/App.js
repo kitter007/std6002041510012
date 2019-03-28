09301a9c941c5f0a33f09e5b9409a6fc46701d60
กิตติพร สินนุรักษ์กูล <s6002041510012@Macmini-213-12.local>
2019-03-28 15:22:59 +0700

    using Card component on App

--- components/App.js
+++ components/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {View,Text} from 'react-native';
 import Header from './Header';
+import Card from './Card';
 
-class App extends React.Component{
+class App extends Component{
     render(){
         return(
             <View>
                 <Header title = {'Albums'}></Header>
                 <Text>Kittiporn Sinnurukkun</Text>
+                <Card/>
             </View>
         );
     }
