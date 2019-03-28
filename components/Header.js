5e7a6f449b6f3c011bba0951beaa8f48e009b70f
กิตติพร สินนุรักษ์กูล <s6002041510012@Macmini-213-12.local>
2019-03-28 14:53:47 +0700

    add Styles Header

--- components/Header.js
+++ components/Header.js
@@ -1,34 +1,29 @@
 import React,{Component} from 'react';
 import {View,Text} from 'react-native';
 
-const styles = {
-    textStyle:{
-        fontSize:25,
-        color :'black'
-    },
-    viewStyle:{
-        justifyContent: 'center',
-        alignItems:'center',
-        padding:15,
-        backgroundColor:'#D0B3E1',
-        shadowColor:'red',
-        shadowOffset:{width:0,height:2},
-        shadowOpacity:100,
-        elevation:10,
-        position:'relative'
-    }
-};
-
 class Header extends Component{
     render(){
         return (
-            <View>
-                <Text>{this.props.title}</Text>
+            <View style={styles.header}>
+                <Text style={styles.text}>{this.props.title}</Text>
             </View>
         );
     }
 }
-export default Header
+export default Header;
+
+const styles = {
+    header:{
+        padding:20,
+        alignItems:'center',
+        backgroundColor:'#D0B3E1'
+    },
+    text:{
+        fontSize:25,
+        color :'black'
+    },
+};
+
 //แบบสั้น
 /*export default (props) =>
     <View style={styles.viewStyle}>
